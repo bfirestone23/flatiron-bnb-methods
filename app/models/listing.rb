67aeb6addc4650ead5c97a653758cb147901b605
Brian Firestone <bfirestone2339@gmail.com>
2021-02-23 22:27:26 -0500

    commit tree 1f4fb71cbbc2b8ac8405a5e3cf9a1beb45ccfee8

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,38 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
+
+  validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood_id
+
+  after_save :set_host
+  before_destroy :unset_host
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
   
+  private 
+
+  def self.available(start_date, end_date)
+    if start_date && end_date
+      joins(:reservations).
+        where.not(reservations: {checkin: start_date..end_date}) &
+      joins(:reservations).
+        where.not(reservations: {checkout: start_date..end_date})
+    else
+      []
+    end
+  end
+
+  def set_host
+    if !host.host?
+      host.update(host: true)
+    end
+  end
+
+  def unset_host
+    if Listing.where(host: host).where.not(id: id).empty?
+      host.update(host: false)
+    end
+  end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,44 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, :checkout, presence: true
+  validate :guest_and_host_not_the_same, :checkout_after_checkin, :available, :checkin_checkout_not_the_same
+
+  def duration
+    (checkout - checkin).to_i
+  end
+
+  def total_price
+    listing.price * duration
+  end
+
+  private
+
+  def guest_and_host_not_the_same
+    if guest_id == listing.host_id
+      errors.add(:guest_id, "You can't book your own apartment.")
+    end
+  end
+
+  def checkout_after_checkin
+    if checkin && checkout && checkout < checkin
+      errors.add(:guest_id, "Your checkout date must be after your checkin date.")
+    end
+  end
+
+  def checkin_checkout_not_the_same
+    if checkin == checkout
+      errors.add(:guest_id, "Your checkin and checkout dates must differ.")
+    end
+  end
+
+  def available
+    Reservation.where(listing_id: listing.id).where.not(id: id).each do |r|
+      booked_dates = r.checkin..r.checkout
+      if booked_dates === checkin || booked_dates === checkout
+        errors.add(:guest_id, "Sorry, this listing isn't available for the selected dates.")
+      end
+    end
+  end
+
 end
